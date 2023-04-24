2ad0477274907a57464f8eb768e416139610b39d
Sirui Liu <90686789+Sirui-Liu-tech@users.noreply.github.com>
2023-04-25 00:58:36 +0800

    Add files via upload

---
+++ mah.py
@@ -0,0 +1,23 @@
+class Player:
+    def __init__(self):
+        self.points = 25000
+        self.home = 0
+        self.win = 0
+        self.for_win = 0
+        self.wind = 1
+
+
+class Round:
+    def __init__(self):
+        self.rwind = 1
+
+
+class Game:
+    def __init__(self):
+        self.winner = 1
+    def over():
+        points = 
+        
+
+
+
